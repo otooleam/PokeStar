a73826065ec8cb2b8574f11c1ac18e00e2db6058
otooleam <otooleam@uwplatt.edu>
2020-06-19 21:46:17 -0500

    Added Libraries; Created Async thread
    Brought Bot Online!!

---
+++ PokeStar/PokeStar/Program.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeStar
+{
+    public class Program
+    {
+        // Allows System to run Asynchronously
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult(); //Any Exceptions get thrown here
+
+        private DiscordSocketClient _client;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            _client.Log += Log;
+
+            //TODO something more secure than token.txt lmao
+            var token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        //TODO set up proper logging framework
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
