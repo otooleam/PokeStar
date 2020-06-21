db3262ee8688410393bc70eb6e666cfc77b9ab24
otooleam <otooleam@uwplatt.edu>
2020-06-21 12:37:17 -0500

    Raid.cs, added RaidBoss.cs

---
+++ PokeStar/PokeStar/DataModels/RaidCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeStar.DataModels
+{
+    class RaidCounter
+    {
+        private string Name { get; set; }
+        private string FastAttack { get; set; }
+        private string ChargedAttack { get; set; }
+    }
+}
