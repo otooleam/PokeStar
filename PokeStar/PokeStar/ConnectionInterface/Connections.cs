5faf1ed5a09b670cd1bd8b8eeb91889753737b1d
birchbr <birchbr@uwplatt.edu>
2020-06-25 22:07:56 -0500

    Added html scrapper

---
+++ PokeStar/PokeStar/ConnectionInterface/Connections.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PokeStar.ConnectionInterface
+{
+   public static class Connections
+   {
+      private const Uri raid_boss_url = new Uri("https://thesilphroad.com/raid-bosses");
+      private const string raid_boss_html = "//*[@class = 'col-md-4']";
+
+      public static Uri RAID_BOSS_URL => raid_boss_url;
+      public static string RAID_BOSS_HTML => raid_boss_html;
+   }
+}
