2d62bd36af7070d689db9edd1b1c6d271592c42f
birchbr <birchbr@uwplatt.edu>
2020-06-25 22:38:57 -0500

    fixed syntax error

--- PokeStar/PokeStar/ConnectionInterface/Connections.cs
+++ PokeStar/PokeStar/ConnectionInterface/Connections.cs
@@ -4,7 +4,7 @@ namespace PokeStar.ConnectionInterface
 {
    public static class Connections
    {
-      private const Uri raid_boss_url = new Uri("https://thesilphroad.com/raid-bosses");
+      private static readonly Uri raid_boss_url = new Uri("https://thesilphroad.com/raid-bosses");
       private const string raid_boss_html = "//*[@class = 'col-md-4']";
 
       public static Uri RAID_BOSS_URL => raid_boss_url;
