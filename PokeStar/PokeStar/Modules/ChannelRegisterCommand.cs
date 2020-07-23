e7f0793648bcc0dbeb5bb492c78542bdf8488e2e
otooleam <otooleam@uwplatt.edu>
2020-07-22 19:48:54 -0500

    runtime channel registration

---
+++ PokeStar/PokeStar/Modules/ChannelRegisterCommand.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeStar.Modules
+{
+   class ChannelRegisterCommand : ModuleBase<SocketCommandContext>
+   {
+      public static Dictionary<ulong, List<ulong>> registeredChannels = new Dictionary<ulong, List<ulong>>();
+
+      //Registers the channel this command is run in as a command channel
+      [Command("register channel")]
+      public async Task Register(string purpose = null)
+      {
+         ulong guild = Context.Guild.Id;
+         ulong channel = Context.Channel.Id;
+
+         if (registeredChannels.Keys.Contains(guild))
+            registeredChannels[guild].Add(channel);
+         else
+         {
+            registeredChannels.Add(guild, new List<ulong> { channel });
+         }
+      }
+   }
+}
