6ad126b3888a4332d80448021195366295fba85d
birchbr <birchbr@uwplatt.edu>
2020-07-30 21:27:41 -0500

    Added Nona DB Connector

---
+++ PokeStar/PokeStar/ConnectionInterface/NONADatabaseConnector.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace PokeStar.ConnectionInterface
+{
+   public class NONADatabaseConnector : DatabaseConnector
+   {
+      public NONADatabaseConnector(string connectionString) : base(connectionString) { }
+   }
+}
