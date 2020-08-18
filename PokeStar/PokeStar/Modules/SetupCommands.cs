5995c7f4f3d369531307f1ea433d63ad82912138
otooleam <otooleam@uwplatt.edu>
2020-08-18 13:17:19 -0500

    Update SetupCommands.cs

--- PokeStar/PokeStar/Modules/SetupCommands.cs
+++ PokeStar/PokeStar/Modules/SetupCommands.cs
@@ -20,16 +20,24 @@ namespace PokeStar.Modules
          if (Environment.GetEnvironmentVariable("SETUP_COMPLETE").Equals("FALSE", StringComparison.OrdinalIgnoreCase))
          {
             if (Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString().Equals("Trainer", StringComparison.OrdinalIgnoreCase)) == null)
+            {
                await Context.Guild.CreateRoleAsync("Trainer", null, new Color(185, 187, 190), false, false, null).ConfigureAwait(false);
+            }
             if (Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString().Equals("Valor", StringComparison.OrdinalIgnoreCase)) == null)
+            {
                await Context.Guild.CreateRoleAsync("Valor", null, new Color(153, 45, 34), false, false, null).ConfigureAwait(false);
+            }
             if (Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString().Equals("Mystic", StringComparison.OrdinalIgnoreCase)) == null)
+            {
                await Context.Guild.CreateRoleAsync("Mystic", null, new Color(39, 126, 205), false, false, null).ConfigureAwait(false);
+            }
             if (Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString().Equals("Instinct", StringComparison.OrdinalIgnoreCase)) == null)
+            {
                await Context.Guild.CreateRoleAsync("Instinct", null, new Color(241, 196, 15), false, false, null).ConfigureAwait(false);
+            }
             Environment.SetEnvironmentVariable("SETUP_COMPLETE", "TRUE");
          }
-         await ReplyAsync("Setup for Nona has been complete.");
+         await ReplyAsync("Setup for Nona has been complete.").ConfigureAwait(false);
       }
    }
 }
