6bb689eea864ae43a61d3bcf72a00bb6c5f97e2e
Brandon Birch <marthyu15@gmail.com>
2020-09-11 23:53:04 -0500

    flipped ready and attending lists

--- PokeStar/PokeStar/ModuleParents/RaidCommandParent.cs
+++ PokeStar/PokeStar/ModuleParents/RaidCommandParent.cs
@@ -605,8 +605,8 @@ namespace PokeStar.ModuleParents
             string invitedAttendList = BuildInvitedList(group.GetReadonlyInvitedAttending());
             string invitedReadyList = BuildInvitedList(group.GetReadonlyInvitedReady());
 
-            embed.AddField($"**{groupPrefix}Ready {ready}/{total}** (Remote {remote}/{Global.LIMIT_RAID_INVITE})", $"{BuildTotalList(attendList, invitedAttendList)}");
-            embed.AddField($"**{groupPrefix}Attending**", $"{BuildTotalList(readyList, invitedReadyList)}");
+            embed.AddField($"**{groupPrefix}Ready {ready}/{total}** (Remote {remote}/{Global.LIMIT_RAID_INVITE})", $"{BuildTotalList(readyList, invitedReadyList)}");
+            embed.AddField($"**{groupPrefix}Attending**", $"{BuildTotalList(attendList, invitedAttendList)}");
          }
          embed.AddField($"**Need Invite:**", $"{BuildRequestInviteList(raid.GetReadonlyInviteList())}");
          embed.WithFooter($"Note: the max number of members in a raid is {Global.LIMIT_RAID}, and the max number of invites is {Global.LIMIT_RAID_INVITE}.");
