c506530e0858ff3ab5ca7a0f4e132b15600e62c1
Amanda <52638577+otooleam@users.noreply.github.com>
2020-10-09 00:04:06 -0500

    Revert "Admin precondition"

--- PokeStar/PokeStar/Modules/SystemEditCommands.cs
+++ PokeStar/PokeStar/Modules/SystemEditCommands.cs
@@ -1,8 +1,6 @@
 ﻿using System.Threading.Tasks;
-using Discord;
 using Discord.Commands;
 using PokeStar.DataModels;
-using PokeStar.PreConditions;
 using PokeStar.ConnectionInterface;
 
 namespace PokeStar.Modules
@@ -15,7 +13,6 @@ namespace PokeStar.Modules
       [Command("prefix")]
       [Summary("Sets the command prefix for this server.")]
       [Remarks("Prefix may only be a single character long.")]
-      [RequireUserPermission(GuildPermission.Administrator)]
       public async Task Prefix([Summary("Prefex to set for commands.")] char prefix)
       {
          Connections.Instance().UpdatePrefix(Context.Guild.Id, prefix.ToString());
@@ -24,8 +21,6 @@ namespace PokeStar.Modules
 
       [Command("updatePokemonNames")]
       [Summary("Updates the saved list of Pokémon names from the database.")]
-      [RequireUserPermission(GuildPermission.Administrator)]
-      [NonaAdmin()]
       public async Task UpdatePokemonNames()
       {
          Connections.Instance().UpdateNameList();
@@ -34,8 +29,6 @@ namespace PokeStar.Modules
 
       [Command("toggleUseEmptyRaid")]
       [Summary("Toggle empty raid feature for all servers.")]
-      [RequireUserPermission(GuildPermission.Administrator)]
-      [NonaAdmin()]
       public async Task UseEmptyRaid()
       {
          Global.USE_EMPTY_RAID = !Global.USE_EMPTY_RAID;
@@ -45,8 +38,6 @@ namespace PokeStar.Modules
 
       [Command("toggleUseNonaTest")]
       [Summary("Toggle accepting messages from Nona Test Bot")]
-      [RequireUserPermission(GuildPermission.Administrator)]
-      [NonaAdmin()]
       public async Task UseNonaTest()
       {
          Global.USE_NONA_TEST = !Global.USE_NONA_TEST;
