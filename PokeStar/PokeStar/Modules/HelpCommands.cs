967fd2ae408005819ccacce4fc17ae792deb3491
Brandon Birch <marthyu15@gmail.com>
2020-11-08 19:06:05 -0600

    Added scrolling help command

--- PokeStar/PokeStar/Modules/HelpCommands.cs
+++ PokeStar/PokeStar/Modules/HelpCommands.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 using Discord;
 using Discord.Commands;
 using Discord.WebSocket;
@@ -15,63 +16,72 @@ namespace PokeStar.Modules
    /// </summary>
    public class HelpCommands : ModuleBase<SocketCommandContext>
    {
+      private static readonly Dictionary<ulong, Tuple<int, List<CommandInfo>>> helpMessages = new Dictionary<ulong, Tuple<int, List<CommandInfo>>>();
+
+      private const int MAX_COMMANDS = 10;
+      private const int BACK_ARROW = 0;
+      private const int FORWARD_ARROR = 1;
+
+      private static readonly Emoji[] extraEmojis = {
+         new Emoji("⬅️"),
+         new Emoji("➡️")
+      };
+
       [Command("help")]
       [Summary("Displays info about commands." +
                "Leave blank to get a list of all commands.")]
       public async Task Help([Summary("(Optional) Get help with this command.")] string command = null)
       {
-         EmbedBuilder embed = new EmbedBuilder();
-         embed.WithColor(Global.EMBED_COLOR_HELP_RESPONSE);
 
          SocketGuildUser user = Context.Guild.Users.FirstOrDefault(x => x.Id == Context.User.Id);
-         bool isAdmin = (user.Roles.Where(role => role.Permissions.Administrator).ToList().Count != 0 || Context.Guild.OwnerId == user.Id) ;
+         bool isAdmin = (user.Roles.Where(role => role.Permissions.Administrator).ToList().Count != 0 || Context.Guild.OwnerId == user.Id);
          bool isNona = Context.Guild.Name.Equals(Global.HOME_SERVER, StringComparison.OrdinalIgnoreCase);
 
          if (command == null)
          {
+            List<CommandInfo> validCommands = Global.COMMAND_INFO.Where(cmdInfo => CheckShowCommand(cmdInfo.Name, isAdmin, isNona)).ToList();
             string prefix = Connections.Instance().GetPrefix(Context.Guild.Id);
-            embed.WithTitle("**Command List**");
-            embed.WithDescription($"List of commands supported by Nona.");
-            foreach (CommandInfo cmdInfo in Global.COMMAND_INFO)
+            
+
+            IUserMessage msg = await ReplyAsync(embed: BuildGeneralHelpEmbed(validCommands.Take(MAX_COMMANDS).ToList(), prefix));
+            if (validCommands.Count > MAX_COMMANDS)
             {
-               if (CheckShowCommand(cmdInfo.Name, isAdmin, isNona))
-               {
-                  embed.AddField($"**{prefix}{cmdInfo.Name}**", cmdInfo.Summary ?? "No description available");
-               }
+               helpMessages.Add(msg.Id, new Tuple<int, List<CommandInfo>>(0, validCommands));
+               await msg.AddReactionsAsync(extraEmojis);
             }
-            embed.WithFooter($"Run \"{prefix}help <command name>\" to get help for a specific command.");
-            await ReplyAsync(embed: embed.Build());
          }
          else if (Global.COMMAND_INFO.FirstOrDefault(x => x.Name.Equals(command, StringComparison.OrdinalIgnoreCase)) is CommandInfo cmdInfo
             && CheckShowCommand(cmdInfo.Name, isAdmin, isNona))
          {
-               embed.WithTitle($"**{command} command help**");
-               embed.WithDescription(cmdInfo.Summary ?? "No description available");
-               if (cmdInfo.Aliases.Count > 1)
+            EmbedBuilder embed = new EmbedBuilder();
+            embed.WithColor(Global.EMBED_COLOR_HELP_RESPONSE);
+            embed.WithTitle($"**{command} command help**");
+            embed.WithDescription(cmdInfo.Summary ?? "No description available");
+            if (cmdInfo.Aliases.Count > 1)
+            {
+               StringBuilder sb = new StringBuilder();
+               foreach (string alias in cmdInfo.Aliases)
                {
-                  StringBuilder sb = new StringBuilder();
-                  foreach (string alias in cmdInfo.Aliases)
+                  if (!alias.Equals(command, StringComparison.OrdinalIgnoreCase))
                   {
-                     if (!alias.Equals(command, StringComparison.OrdinalIgnoreCase))
-                     {
-                        sb.Append($"{alias}, ");
-                     }
+                     sb.Append($"{alias}, ");
                   }
-                  embed.AddField("Alternate Command:", sb.ToString().TrimEnd().TrimEnd(','));
                }
-               if (cmdInfo.Remarks != null)
-               {
-                  embed.AddField("**Additional Information:**", cmdInfo.Remarks);
-               }
-               foreach (ParameterInfo param in cmdInfo.Parameters)
-               {
-                  embed.AddField($"**<{param.Name}>**", param.Summary ?? "No description available");
-               }
-               if (cmdInfo.Parameters.Count == 0)
-               {
-                  embed.WithFooter("*This command does not take any parameters.");
-               }
-               await ReplyAsync(embed: embed.Build());
+               embed.AddField("Alternate Command:", sb.ToString().TrimEnd().TrimEnd(','));
+            }
+            if (cmdInfo.Remarks != null)
+            {
+               embed.AddField("**Additional Information:**", cmdInfo.Remarks);
+            }
+            foreach (ParameterInfo param in cmdInfo.Parameters)
+            {
+               embed.AddField($"**<{param.Name}>**", param.Summary ?? "No description available");
+            }
+            if (cmdInfo.Parameters.Count == 0)
+            {
+               embed.WithFooter("*This command does not take any parameters.");
+            }
+            await ReplyAsync(embed: embed.Build());
          }
          else
          {
@@ -99,6 +109,76 @@ namespace PokeStar.Modules
       {
          return (!Global.HIDDEN_COMMANDS.Contains(command) && !Global.ADMIN_COMMANDS.Contains(command) && !Global.NONA_ADMIN_COMMANDS.Contains(command))
                   || (isAdmin && Global.ADMIN_COMMANDS.Contains(command)) || (isAdmin && isNona && Global.NONA_ADMIN_COMMANDS.Contains(command));
+      }
+
+      /// <summary>
+      /// Checks if a message is a help message.
+      /// </summary>
+      /// <param name="id">Id of the message.</param>
+      /// <returns>True if the message is a help message, otherwise false.</returns>
+      public static bool IsHelpMessage(ulong id)
+      {
+         return helpMessages.ContainsKey(id);
+      }
+
+      /// <summary>
+      /// Handles a reaction on a help message.
+      /// </summary>
+      /// <param name="message">Message that was reacted on.</param>
+      /// <param name="reaction">Reaction that was sent.</param>
+      /// <returns>Completed Task.</returns>
+      public static async Task HelpMessageReactionHandle(IMessage message, SocketReaction reaction, ulong guildId)
+      {
+         Tuple<int, List<CommandInfo>> helpMessage = helpMessages[message.Id];
+         int offset = helpMessage.Item1;
+         string prefix = Connections.Instance().GetPrefix(guildId);
+
+         if (reaction.Emote.Equals(extraEmojis[BACK_ARROW]))
+         {
+            if (offset > 0)
+            {
+               offset--;
+               SocketUserMessage msg = (SocketUserMessage)message;
+               await msg.ModifyAsync(x =>
+               {
+                  x.Embed = BuildGeneralHelpEmbed(helpMessage.Item2.Skip(offset * MAX_COMMANDS).Take(MAX_COMMANDS).ToList(), prefix);
+               });
+            }
+         }
+         else if (reaction.Emote.Equals(extraEmojis[FORWARD_ARROR]))
+         {
+            if (helpMessage.Item2.Count > ((offset + 1) * MAX_COMMANDS))
+            {
+               offset++;
+               SocketUserMessage msg = (SocketUserMessage)message;
+               await msg.ModifyAsync(x =>
+               {
+                  x.Embed = BuildGeneralHelpEmbed(helpMessage.Item2.Skip(offset * MAX_COMMANDS).Take(MAX_COMMANDS).ToList(), prefix);
+               });
+            }
+         }
+         helpMessages[message.Id] = new Tuple<int, List<CommandInfo>>(offset, helpMessage.Item2);
+         await message.RemoveReactionAsync(reaction.Emote, (SocketGuildUser)reaction.User);
+      }
+
+      /// <summary>
+      /// Builds a general help embed.
+      /// </summary>
+      /// <param name="commands">List of commands to display.</param>
+      /// <param name="prefix">Prefix used for the server.</param>
+      /// <returns>Embed for viewing a General list of commands.</returns>
+      private static Embed BuildGeneralHelpEmbed(List<CommandInfo> commands, string prefix)
+      {
+         EmbedBuilder embed = new EmbedBuilder();
+         embed.WithTitle("**Command List**");
+         embed.WithDescription($"List of commands supported by Nona.");
+         foreach (CommandInfo cmdInfo in commands)
+         {
+            embed.AddField($"**{prefix}{cmdInfo.Name}**", cmdInfo.Summary ?? "No description available");
+         }
+         embed.WithColor(Global.EMBED_COLOR_HELP_RESPONSE);
+         embed.WithFooter($"Run \"{prefix}help <command name>\" to get help for a specific command.");
+         return embed.Build();
       }
    }
 }
