971ba967c635dd0b8d0aa0c802e6ce0d1e4b9af8
Brandon Birch <marthyu15@gmail.com>
2020-12-01 13:14:23 -0600

    Fixed call issues

--- PokeStar/PokeStar/ConnectionInterface/Connections.cs
+++ PokeStar/PokeStar/ConnectionInterface/Connections.cs
@@ -259,9 +259,9 @@ namespace PokeStar.ConnectionInterface
       {
          pokemon.Forms = POGODBConnector.GetPokemonByNumber(pokemon.Number);
 
-         Tuple<Dictionary<string, int>, Dictionary<string, int>> typeRelations = GetTypeDefenseRelations(pokemon.Type);
-         pokemon.Weakness = typeRelations.Item2.Keys.ToList();
-         pokemon.Resistance = typeRelations.Item1.Keys.ToList();
+         TypeRelation typeRelations = GetTypeDefenseRelations(pokemon.Type);
+         pokemon.Weakness = typeRelations.Weak.Keys.ToList();
+         pokemon.Resistance = typeRelations.Strong.Keys.ToList();
          pokemon.Weather = GetWeather(pokemon.Type);
          pokemon.FastMove = POGODBConnector.GetPokemonMoves(pokemon.Name, Global.FAST_MOVE_CATEGORY);
          pokemon.ChargeMove = POGODBConnector.GetPokemonMoves(pokemon.Name, Global.CHARGE_MOVE_CATEGORY, pokemon.Shadow);
